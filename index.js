4ea479db1daa07e81d2af1cea44b107ab508f900
Aaron pinto <20100312@mail.wit.ie>
2023-05-17 18:14:39 +0000

    Added Accounts Endpoints

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './src/movies';
+import createAccountsRouter from './src/accounts/routes';
+import buildDependencies from "./src/config/dependencies";
 
 dotenv.config();
 
 const app = express();
 
+// Middleware for parsing the body of the request.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = process.env.PORT;
 
+const dependencies = buildDependencies();
+
 app.use('/api/movies', moviesRouter);
+
+app.use('/api/accounts', createAccountsRouter(dependencies));
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
